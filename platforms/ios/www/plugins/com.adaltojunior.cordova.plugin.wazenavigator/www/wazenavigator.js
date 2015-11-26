e2c17d58aa30e57d817a6f0d5f59eb9ca55fc0fb
aie <aiesa.saad@gmail.com>
2015-11-27 01:21:45 +0800

    problem

---
+++ platforms/ios/www/plugins/com.adaltojunior.cordova.plugin.wazenavigator/www/wazenavigator.js
@@ -0,0 +1,30 @@
+cordova.define("com.adaltojunior.cordova.plugin.wazenavigator.WazeNavigator", function(require, exports, module) { /**
+ * Created by adaltojunior on 6/11/15.
+ */
+WazeNavigator = {
+    navigateByPosition: function (position) {
+
+        try{
+            if (!isNaN(position.lat) && !isNaN(position.lng)) {
+                cordova.exec(
+                    function () {
+                        console.log('Good navigation!!!');
+                    },
+                    function () {
+                        console.log('Sorry, there are problems to navigate!!!');
+                    },
+                    'WazeNavigator',
+                    'navigateByWaze',
+                    [position.lat, position.lng]);
+            } else {
+                console.log("Latitude and longitude aren't numbers.");
+            }
+        } catch (e) {
+            console.log("Error on navigate by position: " + e.message);
+        }
+
+    }
+
+};
+module.exports = WazeNavigator;
+});
